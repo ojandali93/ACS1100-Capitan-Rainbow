4947f83fce23d4c8868ad0f77b652653979c2c70
OmarJ <omarjandali93@gmail.com>
2021-09-20 20:23:12 -0700

    complated the captain rainbow checklist

--- checklist/checklist.py
+++ checklist/checklist.py
@@ -32,8 +32,15 @@ def list_all_items():
         print(str(index) + ' - ' + item)
         index += 1
 
-def mark_completed(index):
-    item = "X " + checklist[index]
+def mark_complete(index):
+    item = "√ " + checklist[index]
+    update(index, item)
+
+def mark_incomplete(index):
+    item = checklist[index]
+    print(item)
+    item.replace("√","X")
+    print(item)
     update(index, item)
 
 def select(function_code):
@@ -53,49 +60,19 @@ def select(function_code):
     elif function_code == "Q":
         return False
 
+    elif function_code == "M":
+        index = input("Index for complete item: ")
+        mark_complete(int(index))
+
+    elif function_code == "I":
+        index = input("Index for incomplete item: ")
+        mark_incomplete(int(index))
+
     else:
         print("Wrong Option")
 
 def test():
-    # create("navy")
-    # create('blue')
-    # create('cyan')
-    # create('green')
-    # create('yellow')
-    # create('red')
-    # create('orange')
-    # create('white')
-    # print(checklist)
-    # read(9)
-    # print(checklist)
-    # destroy(9)
-    # print(checklist)
-    # update(8, 'red')
-    # update(7, 'orange')
-    # print(checklist)
-    # print('-------testing list items-----------')
-    # create("purple sox")
-    # create("red cloak")
-    # create("green sox")
-    # create("yellow sox")
-    # print(read(0))
-    # print(read(1))
-    # update(0, "purple socks")
-    # destroy(1)
-    # print(read(0))
-    # list_all_items()
-    # mark_completed(1)
-    # list_all_items()
-    # print('-------testing user items-----------')
-    # # Call your new function with the appropriate value
-    # select("C")
-    # # View the results
-    # list_all_items()
-    # # Call function with new value
-    # select("R")
-    # # View results
-    # list_all_items()
-    # # Continue until all code is run
+    # insert testing below
     print('------ Testing Loop ------')
 
 test()
@@ -103,5 +80,5 @@ test()
 running = True
 while running:
     selection = input(
-        "Press C to add to list, R to Read from list and P to display list: ")
+        "Press C to add to list, R to Read from list, P to display list, M to mark completed and I to mark incompleted: ")
     select(selection)
